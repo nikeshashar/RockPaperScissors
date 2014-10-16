7620371eb6292771e4119c2cbd3d5eed945f8d41
nikesh <nikeshashar@gmail.com>
2014-10-16 17:08:22 +0100

    Resetting Github and upload to new repo

---
+++ server.rb
@@ -0,0 +1,47 @@
+require 'sinatra'
+require './lib/player'
+require './lib/game'
+
+
+class RockPaperScissors < Sinatra::Base
+
+
+  set :root, "#{File.dirname(__FILE__)}"
+  set :public_folder, Proc.new { File.join(root, "public") }
+
+  get '/' do
+    erb :index
+  end
+
+  get '/new-game' do
+  	erb :new_player
+  end
+
+  post '/register' do 
+  	@player = params[:name]
+  	erb :play	
+  end
+
+  post "/play" do
+  	player = Player.new(params[:name])
+  	player.picks = params[:pick]
+  	computer = generate_computer
+  	@game = Game.new(player, computer)
+  	erb :outcome
+  end
+
+  get "/restart_game" do
+    redirect to ('/play')
+  end
+
+  def generate_computer
+  	choice = ["Rock","Paper","Scissors"].sample
+
+  	comp = Player.new("computer")
+  	comp.picks = choice
+  	comp
+  end
+
+  # start the server if ruby file executed directly
+  run! if app_file == $0
+end
